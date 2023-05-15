cff6073ed7a4543a36ee8f494a6e3b754f46c099
4862abd <4862abd@gmail.com>
2023-05-15 10:21:40 +0900

    [POST] UPDATE

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230512.175636';
+const cacheName = 'chirpy-20230515.102058';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
