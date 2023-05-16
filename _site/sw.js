f06acb612e58a66017e45f751a03deb4227f8a26
4862abd <4862abd@gmail.com>
2023-05-16 19:41:21 +0900

    [SYSTEM] NEW

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230516.190456';
+const cacheName = 'chirpy-20230516.183926';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
